7b3e72c5326287acd4404c78336d1fc5797b43cd
shunnoda <163516299+shunnoda@users.noreply.github.com>
2024-04-30 17:06:27 +0900

    Add files via upload

---
+++ kadai_007/calc.js
@@ -0,0 +1,15 @@
+// 変数numに0～4までのランダムな整数を代入する
+let num = 5;
+
+if ( num%3 === 0 && num%5 === 0){
+    console.log("3と5の倍数です");
+}
+else if ( num%3 === 0 ){
+    console.log("3の倍数です");
+}
+else if ( num%5 === 0 ){
+    console.log("5の倍数です");
+}
+else{
+    console.log(num);
+}
